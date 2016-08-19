d901fe136d22a5270a94f8c657b69f9258e6e2d3
gaozhan <494039504@qq.com>
2016-08-19 11:20:02 +0800

    demo

---
+++ app/src/main/java/com/example/gao/resourceloaderapk1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gao.resourceloaderapk1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by gao on 2016/8/10.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
---
+++ app/src/main/java/com/example/gao/resourceloaderapk1/UIUtil.java
@@ -0,0 +1,18 @@
+package com.example.gao.resourceloaderapk1;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class UIUtil {
+	
+	public static String getTextString(Context ctx){
+
+		return ctx.getResources().getString(R.string.app_name);
+	}
+	
+	public static Drawable getImageDrawable(Context ctx){
+		return ctx.getResources().getDrawable(R.drawable.icon);
+	}
+}
